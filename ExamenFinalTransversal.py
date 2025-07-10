6b5be76b99fdde8222ecfb157043954a7657169d
OscarMs1 <Oa.munoz@duocuc.cl>
2025-07-10 11:57:53 -0400

    Add files via upload

---
+++ ExamenFinalTransversal.py
@@ -0,0 +1,55 @@
+productos = {'8475HD': ['HP', 15.6, '8GB', 'DD', '1T', 'Intel Core i5', 'Nvidia GTX1050'],
+'2175HD': ['Acer', 14, '4GB', 'SSD', '512GB', 'Intel Core i5', 'Nvidia GTX1050'],
+'JjfFHD': ['Asus', 14, '16GB', 'SSD', '256GB', 'Intel Core i7', 'Nvidia RTX2080Ti'],
+'fgdxFHD': ['HP', 15.6, '12GB', 'DD', '1T', 'Intel Core i3', 'integrada'],
+'GF75HD': ['Asus', 15.6, '12GB', 'DD', '1T', 'Intel Core i7', 'Nvidia GTX1050'],
+'123FHD': ['Acer', 14, '6GB', 'DD', '1T', 'AMD Ryzen 5', 'integrada'],
+'342FHD': ['Acer', 15.6, '8GB', 'DD', '1T', 'AMD Ryzen 7', 'Nvidia GTX1050'],
+'UWU131HD': ['Dell', 15.6, '8GB', 'DD', '1T', 'AMD Ryzen 3', 'Nvidia GTX1050'], 
+}
+
+stock = {'8475HD': [387990,10], '2175HD': [327990,4], 'JjfFHD': [424990,1],
+'fgdxFHD': [664990,21], '123FHD': [290890,32], '342FHD': [444990,7],
+'GF75HD': [749990,2], 'UWU131HD': [349990,1], 'FS1230HD': [249990,0], }
+
+
+while = True
+
+def stock_marca():
+    int(input("Ingrese la marca del stock a preguntar"))
+    for stock_marca(marca) in stock
+    datos = stock.lower [1] stock_marca(marca).lower[0]
+    if datos = None
+    print("no se ha encontrado ningun dato respecto al stock de {stock_marca}")
+    else:
+    print("error, intente nuevamente ")
+    break 
+def busqueda_por_precio(): = bpp 
+busqueda_por_precio =>0
+marcamodelo = list 
+for bpp in busqueda_por_precio
+int(input("ingrese el precio a buscar"))
+precio = marcamodelo.__len__
+def eliminar_producto() = borrar 
+    int(input("Ingrese producto a eliminar: "))
+    if eliminar_producto = False
+    for in eliminar_producto in productos
+    if productos = True 
+    print("!Producto eliminado¡.")
+    if not eliminar_producto:
+        print("no se ha eliminado ningun producto")
+    print ("========== MENU ===========")
+    print ("1.- stock marca")
+    print ("2.- busqueda por precio")
+    print ("3.- eliminar producto")
+    print ("4.- salir")
+    print ("===========================")
+    opc = input("Ingrese una opcion")
+    if "opc1":
+    print("Ingrese marca a saber por stock")
+    else "opc2":
+    print ("Busque por precio")
+    else "opc3"
+    print ("Esta eliminando un producto")
+    else "opc4"
+    print ("progama finalizado.")
